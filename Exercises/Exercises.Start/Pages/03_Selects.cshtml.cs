41ed6b0a01d402f6e14830fd5d323f46e0098f6a
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2023-11-22 22:39:20 +0545

    Completed Selects Program

--- Exercises/Exercises.Start/Pages/03_Selects.cshtml.cs
+++ Exercises/Exercises.Start/Pages/03_Selects.cshtml.cs
@@ -25,19 +25,25 @@ namespace Exercises.Pages
                 var items = cuisines.Keys
                     .Select(c => new SelectListItem(c, c))
                     .ToList();
-                
+
                 items.Insert(0, new SelectListItem("Choose an option", "") {
                     Disabled = true,
                     Selected = true
                 });
-                
+
                 return items;
             }
         }
-        
+
         // Step #1
         // TODO: Add string? parameters Cuisine and Food
         // hint: don't forget BindProperty
+
+        [BindProperty(SupportsGet =true)]
+        public string? Cuisine {  get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Food { get; set; }
 
         public void OnGet()
         {
@@ -46,22 +52,22 @@ namespace Exercises.Pages
         public IActionResult OnGetFoods()
         {
             var html = new StringBuilder();
-            
+
             // Step #2
             // Todo: Generate Options for Select Element
-            // hint: use the selected cuisine to get the 
-            
-            #region Selection logic 
-            /*
+            // hint: use the selected cuisine to get the
+
+            #region Selection logic
+
             if (Cuisine is { Length: > 0 } cuisine && cuisines.TryGetValue(cuisine, out var foods))
             {
                 html.AppendLine("<option disabled selected>Select a food</option>");
-                foreach (var food in foods) 
+                foreach (var food in foods)
                 {
                     html.AppendLine($"<option>{food}</option>");
-                }    
+                }
             }
-             */
+
             #endregion
 
             return Content(html.ToString(), "text/html");
@@ -72,8 +78,8 @@ namespace Exercises.Pages
             // Step 3
             // Todo: Return Span proclaiming your love for Food
             // hint: <span><i class=\"fa fa-heart\"></i> I love {Food}!</span>
-            
-            return Content($"");
+
+            return Content($"<span><i class='fa fa-heart'></i> I love {Food}!</span>");
         }
     }
 }
